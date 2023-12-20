9b254ad7532170744df860355a698af0bd8bd93e
Michael Martin <flrgh@protonmail.com>
2023-12-20 09:25:08 -0800

    test(nameservers): add unit test for resolv.conf parser

--- src/util.rs
+++ src/util.rs
@@ -1,41 +1,35 @@
 use crate::types::IpAddr;
 use std::fs;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
+
+fn impl_try_parse_resolv_conf<T: Read>(buf: T) -> Vec<IpAddr> {
+    BufReader::new(buf)
+        .lines()
+        .map_while(Result::ok)
+        .filter_map(|line| {
+            let line = line.trim();
+
+            if line.is_empty() || line.starts_with('#') {
+                return None;
+            }
+
+            let mut parts = line.split_whitespace();
+
+            match (parts.next(), parts.next(), parts.next()) {
+                (Some("nameserver"), Some(addr), None) => addr.parse::<IpAddr>().ok(),
+                _ => None,
+            }
+        })
+        .take(11) // resty-cli stops adding nameservers after it has > 10
+        .collect()
+}
 
 pub(crate) fn try_parse_resolv_conf() -> Vec<IpAddr> {
-    let mut nameservers = vec![];
+    let Ok(file) = fs::File::open("/etc/resolv.conf") else {
+        return vec![];
+    };
 
-    if let Ok(file) = fs::File::open("/etc/resolv.conf") {
-        BufReader::new(file)
-            .lines()
-            .map_while(Result::ok)
-            .for_each(|line| {
-                let line = line.trim();
-                let mut parts = line.split_whitespace();
-
-                let predicate = match parts.next() {
-                    Some("nameserver") => parts.next(),
-                    _ => return,
-                };
-
-                let ns = match predicate {
-                    Some(s) => s,
-                    // not enough parts
-                    _ => return,
-                };
-
-                // too many parts
-                if parts.next().is_some() {
-                    return;
-                }
-
-                if let Ok(addr) = ns.parse::<IpAddr>() {
-                    nameservers.push(addr);
-                }
-            });
-    }
-
-    nameservers
+    impl_try_parse_resolv_conf(file)
 }
 
 pub(crate) fn split_shell_args<T: AsRef<str> + ?Sized>(s: &T) -> Vec<String> {
@@ -162,5 +156,106 @@ mod tests {
         let resplit = split_shell_args(&rejoined);
         assert_eq!(joined, rejoined);
         assert_eq!(args, resplit);
+    }
+
+    #[test]
+    fn test_impl_try_parse_resolv_conf() {
+        fn addr<T: AsRef<str>>(s: T) -> IpAddr {
+            s.as_ref().parse().unwrap()
+        }
+
+        fn addrs<T: AsRef<str>>(s: Vec<T>) -> Vec<IpAddr> {
+            s.iter().map(addr).collect()
+        }
+
+        let input = r##"# This is /run/systemd/resolve/stub-resolv.conf managed by man:systemd-resolved(8).
+# Do not edit.
+#
+# This file might be symlinked as /etc/resolv.conf. If you're looking at
+# /etc/resolv.conf and seeing this text, you have followed the symlink.
+#
+# This is a dynamic resolv.conf file for connecting local clients to the
+# internal DNS stub resolver of systemd-resolved. This file lists all
+# configured search domains.
+#
+# Run "resolvectl status" to see details about the uplink DNS servers
+# currently in use.
+#
+# Third party programs should typically not access this file directly, but only
+# through the symlink at /etc/resolv.conf. To manage man:resolv.conf(5) in a
+# different way, replace this symlink by a static file or a different symlink.
+#
+# See man:systemd-resolved.service(8) for details about the supported modes of
+# operation for /etc/resolv.conf.
+
+nameserver 127.0.0.53
+options edns0 trust-ad
+search example.com"##;
+
+        assert_eq!(
+            vec![addr("127.0.0.53")],
+            impl_try_parse_resolv_conf(input.as_bytes())
+        );
+
+        let input = "nameserver 127.0.0.53";
+
+        assert_eq!(
+            vec![addr("127.0.0.53")],
+            impl_try_parse_resolv_conf(input.as_bytes())
+        );
+
+        let input = r##"
+_nameserver 127.0.0.1
+nameserver 127.0.0.2
+nameserver 127.0.0.3 oops extra stuff
+"##;
+
+        assert_eq!(
+            vec![addr("127.0.0.2")],
+            impl_try_parse_resolv_conf(input.as_bytes())
+        );
+
+        let input = r##"
+nameserver 127.0.0.1
+nameserver 127.0.0.2
+nameserver 127.0.0.3
+"##;
+
+        assert_eq!(
+            addrs(vec!["127.0.0.1", "127.0.0.2", "127.0.0.3",]),
+            impl_try_parse_resolv_conf(input.as_bytes())
+        );
+
+        let input = r##"
+nameserver 127.0.0.1
+nameserver 127.0.0.2
+nameserver 127.0.0.3
+nameserver 127.0.0.4
+nameserver 127.0.0.5
+nameserver 127.0.0.6
+nameserver 127.0.0.7
+nameserver 127.0.0.8
+nameserver 127.0.0.9
+nameserver 127.0.0.10
+nameserver 127.0.0.11
+nameserver 127.0.0.12
+"##;
+
+        assert_eq!(
+            addrs(vec![
+                "127.0.0.1",
+                "127.0.0.2",
+                "127.0.0.3",
+                "127.0.0.4",
+                "127.0.0.5",
+                "127.0.0.6",
+                "127.0.0.7",
+                "127.0.0.8",
+                "127.0.0.9",
+                "127.0.0.10",
+                "127.0.0.11",
+            ]),
+            impl_try_parse_resolv_conf(input.as_bytes())
+        );
     }
 }
